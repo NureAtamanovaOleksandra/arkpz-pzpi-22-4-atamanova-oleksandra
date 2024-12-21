f2f084ce2d538e034ccdf1bb8bfea826176a1413
NureAtamanovaOleksandra <oleksandra.atamanova@nure.ua>
2024-12-21 18:49:50 +0200

    Add Mail API

--- Task3/arkpz-pzpi-22-4-atamanova-oleksandra-task3/routes/userRoutes.js
+++ Task3/arkpz-pzpi-22-4-atamanova-oleksandra-task3/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
+const sendMail = require('../utils/mail');
 const User = require('../models/User');
 const authenticateToken = require('../middlewares/authenticateToken');
 const checkAdmin = require('../middlewares/checkAdmin');
+const bcrypt = require('bcryptjs');
 
 router.get('/', async (req, res) => {
     try {
@@ -49,6 +52,26 @@ router.delete('/admin/:id', authenticateToken, checkAdmin, async (req, res) => {
     }
 });
 
+router.put('/update-profile', authenticateToken, async (req, res) => {
+    try {
+        const { email, name, newPassword } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        user.name = name || user.name;
+        if (newPassword) {
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            user.password = hashedPassword;
+        }
+
+        const updatedUser = await user.save();
+        res.json({ message: 'Profile updated successfully', user: updatedUser });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(400).json({ message: error.message });
+    }
+});
+
 router.put('/:id', async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
@@ -64,6 +87,25 @@ router.put('/:id', async (req, res) => {
         res.json(updatedUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
+    }
+});
+
+router.post('/forgot-password', async (req, res) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        const newPassword = crypto.randomBytes(8).toString('hex');
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        await user.save();
+
+        await sendMail(user.email, 'Password Reset', `Your new password is: ${newPassword}`);
+
+        res.json({ message: 'New password sent to your email' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
